9c6343ce29a10cfd84269edb872500bcda37a6dc
ASHA LEARNOLOGY <ashalearnology.inst@gmail.com>
2024-10-24 14:00:13 +0530

    go

--- MongoosePractice/src/models/productsModel.js
+++ MongoosePractice/src/models/productsModel.js
@@ -1,22 +1,25 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  product_name: { type: String, required: true },
-  category: { type: String },
-  price: { type: Number },
-  in_stock: { type: Boolean, default: true },
-  rating: { type: Number, min: 0, max: 5 },
-  brand: { type: String },
-  color: { type: String },
-  warranty_years: { type: Number },
-  shipping_cost: { type: Number },
-  address: { type: [String] },
+const productSchema = new mongoose.Schema(
+  {
+    product_name: { type: String, required: true },
+    category: { type: String },
+    price: { type: Number },
+    in_stock: { type: Boolean, default: true },
+    rating: { type: Number, min: 0, max: 5 },
+    brand: { type: String },
+    color: { type: String },
+    warranty_years: { type: Number },
+    shipping_cost: { type: Number },
+    address: { type: [String] },
 
-  //   name: { type: String, required: true },
-  //   email: { type: String, required: true, unique: true },
-  //   password: { type: String, required: true },
-});
+    //   name: { type: String, required: true },
+    //   email: { type: String, required: true, unique: true },
+    //   password: { type: String, required: true },
+  },
+  { collection: "dummyData" }
+);
 
 // const User = mongoose.model("users", productSchema);
-const User = mongoose.model("dummyData", productSchema);
+const User = mongoose.model("dummy", productSchema);
 module.exports = User;
