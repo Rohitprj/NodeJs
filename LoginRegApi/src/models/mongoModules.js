af57fa414ac0e656611571147f1a63b86d850d91
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-05 14:25:53 +0530

    add missing newline in mongoModules.js

--- LoginRegApi/src/models/mongoModules.js
+++ LoginRegApi/src/models/mongoModules.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+
 const loginSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: Number, required: true, unique: true },
