6ad792c09a53f57e00f27017db75dd662430a859
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-05 16:52:45 +0530

    add console log for successful database connection in mongoConnect.js

--- LoginRegApi/src/config/mongoConnect.js
+++ LoginRegApi/src/config/mongoConnect.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 function connectDB() {
   try {
     mongoose.connect("mongodb://localhost:27017/E-Commerce");
+    console.log("DB working");
   } catch (e) {
     console.log("not working", e);
   }
